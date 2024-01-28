fcc4c206a513f1881887f978b2542bcf43d55d9b
clyde <yamaradwayne2@gmail.com>
2024-01-28 17:19:03 +0800

    Test 5

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Window window = Window.get();
         window.run();
 
+        
         // DEBUG
         // WindowFont windowFont = new WindowFont();
         // windowFont.run();
